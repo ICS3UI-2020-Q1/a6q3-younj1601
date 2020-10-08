a43242a0384372d535bc1daa678b006768958528
younj1601 <younj1601@wrdsb.ca>
2020-10-08 17:40:09 +0000

    Finished the third task for 2.04

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Outputs the biggest number in an array of 10 numbers
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,33 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //scanner for input
+    Scanner input = new Scanner(System.in);
+
+    //create an array for numbers
+    int[] nums = new int[10];
+
+    //ask for intergers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    //get numbers from the user and store them in array
+    for(int i = 0; i < nums.length; i++){
+      nums[i] = input.nextInt();
+    }
+
+    int bigNum = nums[0];
+
+    //find the largest number in array
+    for(int i = 0; i < 10; i++){
+      if(nums[i] > bigNum){
+        bigNum = nums[i];
+      }
+    }
+
+    System.out.println("The largest number is " + bigNum);
+
+
+
     
   }
 }
